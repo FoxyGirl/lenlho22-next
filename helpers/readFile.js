03a138aabe7d1e08d20df9b554a271eeefdf3cd8
Elena Pavlova <lenlho22@gmail.com>
2021-01-29 20:54:38 +0300

    Remove console.log

--- helpers/readFile.js
+++ helpers/readFile.js
@@ -1,7 +1,6 @@
 const fs = require("fs").promises;
 
 const readFileAsync = async (fileName) => {
-  console.log("Async");
   try {
     const source = await fs.readFile(`./data/${fileName}`, "utf-8");
     const data = source ? JSON.parse(source) : []; // handle logic with empty files
--- helpers/writeFile.js
+++ helpers/writeFile.js
@@ -1,10 +1,8 @@
 const fs = require("fs").promises;
 
 const writeFileAsync = async (fileName, data) => {
-  console.log("Async");
   try {
     await fs.writeFile(`./data/${fileName}`, JSON.stringify(data));
-    console.log("File written Async successfully\n");
   } catch (error) {
     console.error(error.message);
   }
