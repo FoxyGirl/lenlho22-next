361af234de325c88dcffeb6a73f8826ceeda31b4
Elena Pavlova <lenlho22@gmail.com>
2021-01-29 23:23:51 +0300

    Init store configuration with redux-saga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+// Core
+import { Provider } from "react-redux";
+
+// Other
+import { useStore } from "../init/store";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const store = useStore(pageProps.initialReduxState);
+  console.log("APP render");
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
