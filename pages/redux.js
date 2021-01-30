2d8f4f3c0cba171a2695af07c8eb2046ce8d044b
Elena Pavlova <lenlho22@gmail.com>
2021-01-30 09:24:39 +0300

    Connect redux on all pages

--- pages/redux.js
+++ pages/redux.js
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux";
 import { initializeStore } from "../init/store";
 import { initilDispatcher } from "../init/initilDispatcher";
+import { profileActions } from "../bus/profile/actions";
 
 export const getServerSideProps = async (context) => {
   // initilDispatcher dispathes commom actions for all pages
   const store = await initilDispatcher(context, initializeStore());
   // store.dispatch(); on the server
+  store.dispatch(profileActions.fillProfile({ lastName: "Wolker" }));
 
   const initialReduxState = store.getState();
 
@@ -17,10 +20,13 @@ export const getServerSideProps = async (context) => {
 
 const Redux = ({ initialReduxState }) => {
   console.log("initialReduxState!", initialReduxState);
+  const { firstName, lastName } = useSelector((state) => state.profile);
 
   return (
     <>
       <h1>Redux page</h1>
+      <h4>First name: {firstName}</h4>
+      <h4>Last name: {lastName}</h4>
     </>
   );
 };
