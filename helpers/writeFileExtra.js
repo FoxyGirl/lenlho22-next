ea74dcf85bb70f732df383dce2f3ee434885b868
Elena Pavlova <lenlho22@gmail.com>
2021-02-27 20:06:01 +0300

    Use fs-extra instead of fs to ensure file existing

---
+++ helpers/writeFileExtra.js
@@ -0,0 +1,17 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+const DIR_PATH = "data/";
+
+const writeFileAsync = async (fileName, data) => {
+  const filePath = path.resolve(DIR_PATH, fileName);
+
+  try {
+    await fs.ensureFile(filePath);
+    await fs.writeJson(filePath, data);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = writeFileAsync;
