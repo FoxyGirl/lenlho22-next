5972689b1b7dc91743bb97e7fca3ea9d6dde616e
Elena Pavlova <lenlho22@gmail.com>
2021-02-28 13:20:32 +0300

    Add localization and base language switcher

--- bus/asteroids/Asteroids/index.js
+++ bus/asteroids/Asteroids/index.js
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
+import { useTranslation } from "next-i18next";
 
 import { selectAsteroids } from "@bus/selectors";
 
 import styles from "./Asteroids.module.scss";
 
 const Asteroids = () => {
+  const { t } = useTranslation();
+
   const asteroids = useSelector(selectAsteroids);
 
   const asteroidsJSX = asteroids.map(({ id, full_name }) => (
@@ -13,7 +16,7 @@ const Asteroids = () => {
 
   return (
     <section>
-      <h1>Asteroids</h1>
+      <h1>{t("home:asteroids")}</h1>
       <ul className={styles.wrap}>{asteroidsJSX}</ul>
     </section>
   );
